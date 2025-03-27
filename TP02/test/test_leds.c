c30c68567f01f4f04c7f4b84f04c9fba602ec48c
FranCDoc <fchiesadoc@gmail.com>
2025-03-27 19:37:00 -0300

    Comments.

--- TP02/test/test_leds.c
+++ TP02/test/test_leds.c
@@ -65,12 +65,11 @@ void test_todos_los_leds_inician_apagados(void){
 
 //! @test Prender un LED individual.
 void test_prender_led_individual(void) {
-   uint16_t leds_virtuales = 0xFFFF;
+   uint16_t leds_virtuales = 0xFFFF; 
    
-   LedsInit(&leds_virtuales);
-   LedsTurnOnSingle(4);
-   // TEST_ASSERT_BIT_HIGH(3, leds_virtuales); // NOTE: este test es menos estricto que TEST_ASSERT_EQUAL_HEX16.
-   TEST_ASSERT_EQUAL_HEX16(0x0008, leds_virtuales); // NOTE: Class 3 - 53:15.
+   LedsInit(&leds_virtuales); // Passes the address of leds_virtuales to the "driver" -> port_address. So port_adress points to leds_virtuales.
+   LedsTurnOnSingle(4); // Sets *port_address = 0x0008 -> since port_address points to leds_virtuales, this updates leds_virtuales.
+   TEST_ASSERT_EQUAL_HEX16(0x0008, leds_virtuales); // NOTE: Class 3 - 53:15. |  Verifies the effect of the function call by checking leds_virtuales.
 }
 
 //! @test Apagar un LED individual.
